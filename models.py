6f914122cff78581427acb622c906d1a00ed9682
mrAkbarov <akbarxojiakbarov653@gmail.com>
2026-05-14 21:48:58 +0500

    product model qoshildi

---
+++ models.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+
+class Product:
+    id: int
+    name: str
+    price: float
+    discount_price: float
+    quantity: int
+    description: str
+    created_at: datetime
